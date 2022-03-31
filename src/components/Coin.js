8a2532d1060ca14cf670684a44fd63e28ff72c7f
addon-divyadarshan <addon.trainee02@hotmail.com>
2022-03-31 15:47:15 +0530

    initial commit

---
+++ src/components/Coin.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Coin({ name, icon, price, symbol }) {
+  return (
+
+    <div className="row">
+    <div className="column">
+      <div className="coin">
+        <h3> {name} </h3>
+        <img src={icon} />
+        <h3> Price: {price}</h3>
+        <h3> Symbol: {symbol}</h3>
+    </div>
+      </div>
+      </div>
+  
+
+  
+  );
+}
+
+export default Coin;
